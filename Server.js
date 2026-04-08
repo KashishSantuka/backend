ed240edde1ea70726abb3ed263eaf27f5b26fc2e
Kashish Santuka <your-email@example.com>
2026-04-08 20:38:41 +0530

    Hello

--- Server.js
+++ Server.js
@@ -1,17 +1,16 @@
 import express from "express";
 import cors from "cors";
-import serverless from "serverless-http";
-import taskRouter from './routes/taskRoute.js';
+import taskRouter from "./route/taskRoute.js";
 
 const app = express();
 
-app.use(cors({
-  origin: "*"
-}));
-
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 app.use("/task", taskRouter);
 
-// ✅ THIS replaces app.listen()
-export const handler = serverless(app);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
